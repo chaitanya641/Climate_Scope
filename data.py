cc77b9bcbbe55a4d4f1c59e2afa9138c260119a7
chaitanya641 <chaitanya.challa56@gmail.com>
2025-11-20 13:32:54 +0530

    Delete data.py

--- data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-print("Loading CSV...")
-
-df = pd.read_csv(r"C:\Users\HP\Downloads\infosys_SpringBoard\GlobalWeatherRepository.csv")
-
-print("First 5 rows:")
-print(df.head())
